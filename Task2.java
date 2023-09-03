daf951f4bb44f26eff03a7f3b15ae69bf7f3da95
cs-vansh <104628209+cs-vansh@users.noreply.github.com>
2023-09-03 22:40:10 +0530

    Update Task2.java

--- Task2.java
+++ Task2.java
@@ -1,62 +1,50 @@
 import java.util.Scanner;
 
-class Task2
-{
-	public static void main(String args[])
-	{
-		Scanner sc=new Scanner(System.in);
-		int total=0;
-		System.out.print("Enter the number of subjects:");
-		int n=sc.nextInt();
-		for(int i=1;i<=n;i++)
-		{
-			System.out.print("Enter the marks of Subject "+i+":");
-			int marks=sc.nextInt();
-			total+=marks;	
-		}
-		System.out.println("Your Total Marks are "+total+" out of "+(n*100));
-	
-		float average = total/n;
-		
-		System.out.println("Average of Your Marks is "+average);
-		
-		if(average>=90.0)
-		{
-			System.out.println("You were Outstanding!!, Your Grade is O+");
-		
-		}	
-		else if(80.0<=average && average<90.0)
-		{
-			System.out.println("Your Grade is A+");
-		
-		}
-		else if(70<=average && average<80.0)
-		{
-			System.out.println("Your Grade is A");
-		
-		}
-		else if(60<=average && average<70.0)
-		{
-			System.out.println("Your Grade is B");
-		
-		}
-		else if(50<=average && average<60.0)
-		{
-			System.out.println("Your Grade is C");
-		
-		}
-		else if(40<=average && average<50.0)
-		{
-			System.out.println("Your Grade is D");
-		
-		}
-		else 
-		{
-			System.out.println("You have Failed,Your Grade is F");
-		
-		}
-		sc.close();
-	}
+class Task2 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int total = 0;
+        System.out.print("Enter the number of subjects:");
+        int n = sc.nextInt();
 
+        for (int i = 1; i <= n; i++) {
+            System.out.print("Enter the marks of Subject " + i + ":");
+            int marks = sc.nextInt();
+            if (marks < 0 || marks > 100) {
+                System.out.println("Invalid marks entered. Marks should be between 0 and 100.");
+                sc.close();
+                return;
+            }
+            total += marks;
+        }
 
+        System.out.println("Your Total Marks are " + total + " out of " + (n * 100));
+
+        float averagePercentage = ((float) total / (n * 100)) * 100;
+
+        System.out.println("Average of Your Marks is " + averagePercentage + "%");
+
+        String grade = calculateGrade(averagePercentage);
+        System.out.println("Your Grade is " + grade);
+
+        sc.close();
+    }
+
+    private static String calculateGrade(float averagePercentage) {
+        if (averagePercentage >= 90.0) {
+            return "O+";
+        } else if (averagePercentage >= 80.0) {
+            return "A+";
+        } else if (averagePercentage >= 70.0) {
+            return "A";
+        } else if (averagePercentage >= 60.0) {
+            return "B";
+        } else if (averagePercentage >= 50.0) {
+            return "C";
+        } else if (averagePercentage >= 40.0) {
+            return "D";
+        } else {
+            return "F";
+        }
+    }
 }
